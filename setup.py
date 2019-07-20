82f877f08ff7fa5ff44052092019b69455cbd3d4
datagutten <datagutten@datagutten.net>
2019-07-20 17:26:45 +0200

    Add content-type for readme

--- setup.py
+++ setup.py
@@ -10,12 +10,13 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django_entur_data',
-    version='0.1',
+    version='0.2',
     packages=find_packages(),
     include_package_data=True,
     license='GPL',
     description='Load XML files with data from entur into a database',
     long_description=README,
+    long_description_content_type='text/markdown',
     url='https://github.com/datagutten/django-entur-data',
     author='Anders Birkenes',
     author_email='datagutten@datagutten.net',
