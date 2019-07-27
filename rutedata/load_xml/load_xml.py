2f7509ab7d4619ce0728fe2e989f89a82f71131e
datagutten <datagutten@datagutten.net>
2019-07-27 22:39:30 +0200

    Add more methods to get data from XML

--- rutedata/load_xml/load_xml.py
+++ rutedata/load_xml/load_xml.py
@@ -1,6 +1,7 @@
 import datetime
 import io
 import os
+import re
 import xml.etree.ElementTree
 import xml.etree.ElementTree
 import zipfile
@@ -8,6 +9,11 @@ from datetime import datetime
 
 import requests
 
+
+def parse_line_id(line_id):
+    # RUT:Line:83
+    matches = re.match(r'([A-Z]+):Line:([0-9A-Z]+)', line_id)
+    return matches
 
 class LoadXml:
     namespaces = {'netex': 'http://www.netex.org.uk/netex'}
@@ -24,6 +30,12 @@ class LoadXml:
         return zip_file
 
     def load_netex(self, file='_RUT_shared_data.xml', district='rut'):
+        """
+
+        :param str|none file:
+        :param district:
+        :return:
+        """
         url = 'https://storage.googleapis.com/marduk-production/outbound/netex/rb_%s-aggregated-netex.zip' % district
         zip_file = self.load_file(url)
         if file:
@@ -31,6 +43,53 @@ class LoadXml:
             return xml.etree.ElementTree.fromstring(xml_bytes)
         else:
             return zip_file
+
+    def find_line_file(self, line_num=None, line_id=None, region='RUT'):
+
+        if line_id is None:
+            wanted_file = '{0}_{0}-Line-{1}'.format(region, line_num)
+        else:
+            matches = parse_line_id(line_id)
+            wanted_file = '{0}_{0}-Line-{1}'.format(matches.group(1),
+                                                    matches.group(2))
+        zip_file = self.load_netex(file=None)
+        for file in zip_file.namelist():
+            if file.find(wanted_file) > -1:
+                return [file, zip_file.read(file)]
+
+    def find_line_num(self, service_journey_id):
+        matches = re.match(r'([A-Z]+):ServiceJourney:([0-9A-Z]+)-.+', service_journey_id)
+        region = matches.group(1)
+        line = matches.group(2)
+        return [region, line]
+
+    def find_service_journey(self, service_journey_id, line=None, region=None, xml_root=None):
+        if xml_root is None:
+            if line is None:
+                matches = re.match(r'([A-Z]+):ServiceJourney:([0-9A-Z]+)-.+', service_journey_id)
+                if not region:
+                    region = matches.group(1)
+                line = matches.group(2)
+            [file_name, file] = self.find_line_file(line, region)
+            root = xml.etree.ElementTree.fromstring(file)
+        else:
+            root = xml_root
+        journey = root.find('.//netex:ServiceJourney[@id="%s"]' %
+                            service_journey_id, self.namespaces)
+        return journey
+
+    def find_passings(self, service_journey_id, line=None, region=None, xml_root=None):
+        journey = self.find_service_journey(service_journey_id, line, region, xml_root=xml_root)
+        return journey.findall('.//netex:TimetabledPassingTime', self.namespaces)
+
+    def stop_point(self, line_id=None, stop_point_id=None, xml_root=None):
+        if xml_root is None:
+            [file_name, file] = self.find_line_file(line_id=line_id)
+            root = xml.etree.ElementTree.fromstring(file)
+        else:
+            root = xml_root
+        point = root.find('.//netex:StopPointInJourneyPattern[@id="%s"]' % stop_point_id, self.namespaces)
+        return self.get(point, 'ScheduledStopPointRef')
 
     def load_tiamat(self, file='Oslo_og_Akershus_latest.zip'):
         url = 'https://storage.googleapis.com/marduk-production/tiamat/%s' % file
